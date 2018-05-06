b46785f24df9e4a4988fce7e713514affd70c89b
Franco Capra <franco.capra@outlook.com>
2018-05-06 17:11:37 -0300

    Implementação Updload arquivos para Amazon S3

--- src/main/java/com/lookfood/backend/resources/AuthResource.java
+++ src/main/java/com/lookfood/backend/resources/AuthResource.java
@@ -5,7 +5,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
